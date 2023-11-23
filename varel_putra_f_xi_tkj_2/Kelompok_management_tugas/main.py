b603cca21dc80d23f4f49b9295d0cf5d4981943f
28Varel <148347103+28Varel@users.noreply.github.com>
2023-11-23 10:49:04 +0700

    Add files via upload

---
+++ varel_putra_f_xi_tkj_2/Kelompok_management_tugas/main.py
@@ -0,0 +1,19 @@
+from flet import *
+import flet as ft
+import tkinter as tk
+from tkinter import simpledialog
+from frontend.appbar_view import appbar_widget
+from frontend.gabungan_view import show_content_merge
+
+
+
+def main(page: Page):
+    page.appbar = appbar_widget(page)
+
+    page.add(
+        show_content_merge(page)
+    )
+    
+    # page.update()
+
+ft.app(target=main)
